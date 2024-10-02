35a86ab0ebea8e84cabf9076bf423d167a46c626
faoudia <fares.aoudia0@gmail.com>
2024-10-02 20:54:54 +0200

    Errors handling

--- src/farm.go
+++ src/farm.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-// Type to store a room's information
+// Room datatype
 type Room struct {
 	name  string
 	x     int
@@ -18,20 +18,56 @@ type Room struct {
 	links []*Room
 }
 
-// Checks for errors
-func check(e error) {
+// Error helping function
+func checkError(e error) {
 	if e != nil {
-		panic(e)
+		log.Fatal(e)
 	}
 }
 
+// Check if room was already saved
+func isRoomThere(room Room, rooms []*Room) bool {
+	for _, r := range rooms {
+		if r.name == room.name {
+			return true
+		}
+	}
+	return false
+}
+
 // Given a line, this function extracts the room's values
-func parseRoom(line string) Room {
+func parseRoom(line string, rooms []*Room) (Room, error) {
 	fields := strings.Split(line, " ")
+
+	// Checking the line's format
+	if len(fields) != 3 {
+		return Room{}, errors.New("ERROR: Incorrect room format")
+	}
+
+	// Room's name
 	name := fields[0]
-	x, _ := strconv.Atoi(fields[1])
-	y, _ := strconv.Atoi(fields[2])
-	return Room{name, x, y, []*Room{}}
+
+	// Converting the first field
+	x, err := strconv.Atoi(fields[1])
+	if err != nil {
+		return Room{}, errors.New("ERROR: Invalid data format for x-coordinate")
+	}
+
+	// Converting the second field
+	y, err := strconv.Atoi(fields[2])
+	if err != nil {
+		return Room{}, errors.New("ERROR: Invalid data format for y-coordinate")
+	}
+
+	// Creating the room
+	r := Room{name, x, y, []*Room{}}
+
+	// Checking if the room is already saved
+	if isRoomThere(r, rooms) {
+		return Room{}, errors.New("ERROR: Duplicate room")
+	}
+
+	return r, nil
 }
 
 // Fetches a room based on its name
@@ -41,23 +77,40 @@ func getRoom(name string, rooms []*Room) (*Room, error) {
 			return room, nil
 		}
 	}
-	return nil, errors.New("Room not found")
+	return nil, errors.New("ERROR: Invalid data format, room not found")
 }
 
 // Establishes the links between different rooms
-func parseLinks(rooms []*Room, links []string) {
+func parseLinks(rooms []*Room, links []string) error {
 	for _, link := range links {
 		roomNames := strings.Split(link, "-")
-		r1, e := getRoom(roomNames[0], rooms)
-		check(e)
-		r2, e := getRoom(roomNames[1], rooms)
-		check(e)
 
+		// Get the first room
+		r1, e := getRoom(roomNames[0], rooms)
+		if e != nil {
+			return errors.New("ERROR: Room doesn't exist")
+		}
+
+		// Get the second room
+		r2, e := getRoom(roomNames[1], rooms)
+		if e != nil {
+			return errors.New("ERROR: Room doesn't exist")
+		}
+
+		// Case when the room is linked to itself
+		if r1 == r2 {
+			return errors.New("ERROR: Room is linked to itself")
+		}
+
+		// Connect the two rooms
 		r1.links = append(r1.links, r2)
 		r2.links = append(r2.links, r1)
 	}
+
+	return nil
 }
 
+// Display all the rooms and their links based on a given slice
 func DisplayRooms(rooms []*Room) {
 	for _, room := range rooms {
 		fmt.Printf("Room: %s \n Links: ", room.name)
@@ -70,35 +123,51 @@ func DisplayRooms(rooms []*Room) {
 
 // Main function for the mapping of the farm
 func FarmMapping(filepath string) []*Room {
+	// Log flags
+	log.SetFlags(0)
+
+	// Slice variables
 	rooms := []*Room{}
 	links := []string{}
+
+	// Handle file opening
 	file, err := os.Open(filepath)
-	check(err)
+	checkError(err)
 	defer file.Close()
 
+	// Scanner to read the file
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
+		// Skip empty lines
 		if len(line) == 0 {
 			continue
 		}
 
-		if strings.HasPrefix(line, "##") {
+		// Skip comment, start or end lines
+		if strings.HasPrefix(line, "#") {
 			continue
 		}
 
+		// Add links
 		if strings.Contains(line, "-") {
 			links = append(links, line)
 			continue
 		}
 
-		room := parseRoom(line)
+		// Parse room data
+		room, err := parseRoom(line, rooms)
+		checkError(err)
+
+		// Adding parsed room data to the slice
 		rooms = append(rooms, &room)
 	}
 
-	parseLinks(rooms, links)
+	// Parsing links
+	e := parseLinks(rooms, links)
+	checkError(e)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
