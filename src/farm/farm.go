b9e08f36fd38ad856bd8db45258489f5db52520a
Romain Decaux <romaindecaux.contact@gmail.com>
2024-10-22 14:50:26 +0200

    creation of getPathCap

--- src/farm/farm.go
+++ src/farm/farm.go
@@ -3,6 +3,7 @@ package farm
 import (
 	"lem-in/src/path"
 	"lem-in/src/room"
+	"slices"
 )
 
 // Farm datatype
@@ -19,4 +20,27 @@ func NewFarm() *Farm {
 		Rooms: []*room.Room{},
 		Paths: []*path.Path{},
 	}
+}
+
+func (f *Farm) GetPathCap() int {
+	var sliceOfStartingRoomsName []string
+	var sliceOfEndingRoomsName []string
+
+	for i := 0; i < len(f.Paths); i++ {
+		tempName := f.Paths[i].Route[0].Name
+		if !slices.Contains(sliceOfStartingRoomsName, tempName) {
+			sliceOfStartingRoomsName = append(sliceOfStartingRoomsName, tempName)
+		}
+		tempName = f.Paths[i].Route[len(f.Paths[i].Route)-1].Name
+		if !slices.Contains(sliceOfEndingRoomsName, tempName) {
+			sliceOfEndingRoomsName = append(sliceOfEndingRoomsName, tempName)
+		}
+	}
+
+	start := len(sliceOfStartingRoomsName)
+	end := len(sliceOfEndingRoomsName)
+	if end > start {
+		return end
+	}
+	return start
 }
