e074ac953012d72afded889ae54c3d3e76de5c2b
Romain Decaux <romaindecaux.contact@gmail.com>
2024-10-23 10:49:47 +0200

    fix getpathcap's values

--- src/farm/sort_path.go
+++ src/farm/sort_path.go
@@ -1,6 +1,7 @@
 package farm
 
 import (
+	"fmt"
 	"slices"
 	"sort"
 )
@@ -17,15 +18,18 @@ func (f *Farm) GetPathCap() {
 	var sliceOfEndingRoomsName []string
 
 	for i := 0; i < len(f.PathObject.Paths); i++ {
-		tempName := f.PathObject.Paths[i].Rooms[0].Name
+		tempName := f.PathObject.Paths[i].Rooms[1].Name
 		if !slices.Contains(sliceOfStartingRoomsName, tempName) {
 			sliceOfStartingRoomsName = append(sliceOfStartingRoomsName, tempName)
 		}
-		tempName = f.PathObject.Paths[i].Rooms[len(f.PathObject.Paths[i].Rooms)-1].Name
+		tempName = f.PathObject.Paths[i].Rooms[len(f.PathObject.Paths[i].Rooms)-2].Name
 		if !slices.Contains(sliceOfEndingRoomsName, tempName) {
 			sliceOfEndingRoomsName = append(sliceOfEndingRoomsName, tempName)
 		}
 	}
+
+	fmt.Println(sliceOfStartingRoomsName)
+	fmt.Println(sliceOfEndingRoomsName)
 
 	start := len(sliceOfStartingRoomsName)
 	end := len(sliceOfEndingRoomsName)
