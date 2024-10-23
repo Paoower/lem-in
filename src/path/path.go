2a0fc90ebc6d97092758a8a80e0581b6eb56dca1
Romain Decaux <romaindecaux.contact@gmail.com>
2024-10-23 11:08:43 +0200

    moved and rename the cost of a path to the path structur

--- src/path/path.go
+++ src/path/path.go
@@ -3,25 +3,25 @@ package path
 import "lem-in/src/room"
 
 type PathObject struct {
-	PathsCap     int
-	Paths        []*Path
-	PathTriggers []int
+	PathsCap int
+	Paths    []*Path
 }
 
 type Path struct {
 	Rooms []*room.Room
+	Cost  int
 }
 
 func NewPathObject() *PathObject {
 	return &PathObject{
-		PathsCap:     0,
-		Paths:        []*Path{},
-		PathTriggers: []int{},
+		PathsCap: 0,
+		Paths:    []*Path{},
 	}
 }
 
 func NewPath(rooms []*room.Room) *Path {
 	return &Path{
 		Rooms: rooms,
+		Cost:  len(rooms),
 	}
 }
