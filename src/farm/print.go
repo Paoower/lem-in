30220f1c7a85911e73a95f1da7a52c4120a3217a
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-11-04 14:57:21 +0100

    add function to print state of the farm

---
+++ src/farm/print.go
@@ -0,0 +1,45 @@
+package farm
+
+import (
+	"fmt"
+	"lem-in/src/room"
+)
+
+func	orderByAntNb(rooms []*room.Room) {
+	var i		int
+	var j		int
+	var temp	*room.Room
+
+	i = 1
+	for i < len(rooms) {
+		j = i
+		for j > 0 && rooms[j - 1].AntNb > rooms[j].AntNb {
+			temp = rooms[j]
+			rooms[j] = rooms[j - 1]
+			rooms[j - 1] = temp
+			j--
+		}
+		i++
+	}
+}
+
+func	(f *Farm) Print() {
+	var rooms		[]*room.Room
+	var	roomsLen	int
+
+	roomsLen = len(f.Rooms)
+	rooms = make([]*room.Room, roomsLen)
+	copy(rooms, f.Rooms)
+	orderByAntNb(rooms)
+	for i, r := range rooms {
+		if (r.AntNb == 0) {
+			continue
+		}
+		fmt.Printf("L%d-%s", r.AntNb, r.Name)
+		if (i != roomsLen - 1) {
+			fmt.Print(" ")
+		} else {
+			fmt.Println()
+		}
+	}
+}
--- src/room/room.go
+++ src/room/room.go
@@ -11,7 +11,7 @@ type Room struct {
 	Name     string
 	X        int
 	Y        int
-	Occupied bool
+	AntNb    int // 0 if none
 	Links    []*Room
 }
 
@@ -38,7 +38,7 @@ func NewRoom(name string, x int, y int) *Room {
 		Name:     name,
 		X:        x,
 		Y:        y,
-		Occupied: false,
+		AntNb:    0,
 		Links:    []*Room{},
 	}
 }
