f96a6f5ecc29a8b16ef377d531b5311048a6e68e
Romain Decaux <romaindecaux.contact@gmail.com>
2024-11-15 12:34:26 +0100

    implementing a fix to an issue that appeared because we swapped to using pointers on solution

--- src/farm/sortpath.go
+++ src/farm/sortpath.go
@@ -58,8 +58,10 @@ func (f *Farm) lookingForEveryPossibleSolution() {
 				if len(solutionSlice[indexSolution].Paths) == nbrOfPaths {
 					for otherPath := range f.Paths {
 						if otherPath != indexPath && IsACompatiblePath(solutionSlice[indexSolution], f.Paths[otherPath]) {
-							solutionSlice = append(solutionSlice, solutionSlice[indexSolution])
-							solutionSlice[indexSolution].Paths = append(solutionSlice[indexSolution].Paths, f.Paths[otherPath])
+							tempSolution := objects.NewSolution()
+							tempSolution.Paths = solutionSlice[indexSolution].Paths
+							tempSolution.Paths = append(tempSolution.Paths, f.Paths[otherPath])
+							solutionSlice = append(solutionSlice, tempSolution)
 						}
 					}
 				}
@@ -105,7 +107,8 @@ func (f *Farm) getRidOfCopy() {
 						f.Solutions = f.Solutions[:indexSecondSolution]
 					} else {
 						f.Solutions = append(f.Solutions[:indexSecondSolution], f.Solutions[indexSecondSolution+1:]...)
-						indexSecondSolution--
+						indexFirstSolution = 0
+						indexSecondSolution = 0
 					}
 				}
 			}
@@ -117,10 +120,11 @@ func (f *Farm) sortSolutions() {
 	for _, s := range f.Solutions {
 		s.Sort()
 		s.GetTriggers()
+		s.SetTotalRooms()
 	}
 }
 
-func (f *Farm) keepingBestSolutions() {
+/*func (f *Farm) keepingBestSolutions() {
 	var bestSolutions []*objects.Solution
 	for nbrOfPath := 1; nbrOfPath <= f.PathsCap; nbrOfPath++ {
 		for indexSolution := range f.Solutions {
@@ -141,13 +145,41 @@ func (f *Farm) keepingBestSolutions() {
 		}
 	}
 	f.Solutions = bestSolutions
+}*/
+
+func (f *Farm) keepingBestSolutions() {
+	var bestSolutions []*objects.Solution
+	for nbrOfPath := 1; nbrOfPath <= f.PathsCap; nbrOfPath++ {
+		for indexSolution := range f.Solutions {
+			sizeBS := len(bestSolutions)
+			nbrPathInThisSolution := len(f.Solutions[indexSolution].Paths)
+			if sizeBS >= nbrPathInThisSolution {
+				index := nbrPathInThisSolution - 1
+				if nbrPathInThisSolution == 1 {
+					if bestSolutions[0].Paths[0].Cost > f.Solutions[indexSolution].Paths[0].Cost {
+						bestSolutions[0] = f.Solutions[indexSolution]
+						for p := range bestSolutions[0].Paths {
+							bestSolutions[0].Paths[p].Print()
+						}
+					}
+				} else if bestSolutions[index].TotalRooms > f.Solutions[indexSolution].TotalRooms {
+					bestSolutions[index] = f.Solutions[indexSolution]
+				}
+			} else if sizeBS == (nbrPathInThisSolution - 1) {
+				bestSolutions = append(bestSolutions, f.Solutions[indexSolution])
+			}
+		}
+	}
+	f.Solutions = bestSolutions
 }
 
 func (f *Farm) ShowSolutions() {
 	for s := range f.Solutions {
-		fmt.Println(f.Solutions[s].Paths)
+		fmt.Println("====================================")
+		fmt.Println(f.Solutions[s])
 		for p := range f.Solutions[s].Paths {
 			f.Solutions[s].Paths[p].Print()
+			fmt.Println("--")
 		}
 	}
 }
